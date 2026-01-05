bfd5b5abb76a49125b5b79dc01f0130bf43a24b2
SREEVINAYAKG <srivinayakg2005@gmail.com>
2026-01-05 22:20:13 +0530

    Day 12

---
+++ boring_apartments/boring_apartments.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class boring_apartments {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String x = sc.next();
+            
+            int digit = x.charAt(0) - '0';
+            int len = x.length();
+            
+            int result = (digit - 1) * 10 + (len * (len + 1)) / 2;
+            System.out.println(result);
+        }
+    }
+    
+}
